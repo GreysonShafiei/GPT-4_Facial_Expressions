8b01c5ec557b7b1d0f09f30485a9975e8a5b29e2
Greyson Shafiei <gshafiei@charlotte.edu>
2025-04-11 19:17:15 -0400

    feat: add automated image rotation

---
+++ ImageRotator.py
@@ -0,0 +1,19 @@
+import os
+from PIL import Image
+
+# Input and output folders
+input_folder = "Images/Single_upright"
+output_folder = "Images/Single_inverted"
+
+# Loop through all PNGs
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(".jpg"):
+        input_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+
+        # Open, rotate, and save
+        with Image.open(input_path) as img:
+            rotated = img.rotate(180)  # You can change angle (e.g., 90, 270)
+            rotated.save(output_path)
+
+print("Successfully rotated the images 180 to: ", output_folder)
